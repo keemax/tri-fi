ab8c181e8308a53133ee3c38ccbbf6a8bdacc061
Gradie <gradiep@amazon.com>
2014-06-14 13:47:46 -0700

    More model changes.  Deprecating TrainingUpdate for TrainingSignature

--- src/main/java/com/zappos/util/TriFiUtils.java
+++ src/main/java/com/zappos/util/TriFiUtils.java
@@ -4,11 +4,12 @@ import com.zappos.model.Location;
 import com.zappos.model.Router;
 
 /**
- * Created by gradiep on 6/13/14.
+ * 
  */
 public class TriFiUtils {
 
     public static final double DEFAULT_NETWORK = 180.0;
+
     /**
      * 10 ^ ((27.55 - (20 * log10(5825)) - (-87))/20)
      */
@@ -18,16 +19,19 @@ public class TriFiUtils {
     }
 
     public static Double getDimensionValue(Location location, String dimension) {
-        if(dimension.equals("x") || dimension.equals("X")) {
-            return location.getX();
-        } else if(dimension.equals("y") || dimension.equals("Y")) {
-            return location.getY();
-        } else if(dimension.equals("floor") || dimension.equals("Floor")) {
-            return location.getFloor();
+        switch (dimension) {
+            case "x":
+            case "X":
+                return location.getX();
+            case "y":
+            case "Y":
+                return location.getY();
+            case "floor":
+            case "Floor":
+                return location.getFloor();
         }
         return null;
     }
-
 }
 
 
